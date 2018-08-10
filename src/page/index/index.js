c7fb79584fc6ad2ed3dea06f1b320bbf574b6d6b
gaocong0511 <qq370010866gc@gmail.com>
2018-08-10 17:29:04 +0800

    编写首页的代码

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -6,11 +6,8 @@
  */
 
 'use strict';
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-require('page/common/nav-side/index.js');
 var navSide = require('page/common/nav-side/index.js');
 var _mall = require('util/mall.js');
-navSide.init({
-    name: 'order-list'
-});
