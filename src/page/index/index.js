7304b7aa6bb4211671a2f613acfe828b7fce97ba
gaocong0511 <qq370010866gc@gmail.com>
2018-08-13 16:06:34 +0800

    完成首页代码的编写

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -9,5 +9,22 @@
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var navSide = require('page/common/nav-side/index.js');
+require('util/slider/index');
 var _mall = require('util/mall.js');
+var bannerTemplate=require('./banner.string');
+
+$(function () {
+    //渲染html
+    var bannerHtml=_mall.renderHtml(bannerTemplate);
+    $('.banner-con').html(bannerHtml);
+
+    //初始化banner
+    var $slider=$('.banner').unslider({
+        dots:true
+    });
+    //前一张按钮和后一张按钮的事件绑定
+    $('.banner-con .banner-arrow').click(function () {
+        var forward=$(this).hasClass('prev')?'prev':'next';
+        $slider.data('unslider')[forward]();
+    })
+})
